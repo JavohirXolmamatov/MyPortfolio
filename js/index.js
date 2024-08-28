006ba5da89b7e2567f8570e9b87ee4e3ec6a052b
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2024-08-28 11:12:37 +0500

    a

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.getElementById('download-btn').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'public/MyResume'; // Rezyume fayl manzili
+    link.href = 'public/MyResume.pdf'; // Rezyume fayl manzili
     link.download = 'MyResume.pdf'; // Yuklab olinadigan fayl nomi
     link.click();
   });
